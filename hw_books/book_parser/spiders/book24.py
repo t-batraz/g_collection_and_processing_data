06ea6a778ae8bff0a7090807ca1bf26a55028f4e
t-batraz <34238336+t-batraz@users.noreply.github.com>
2020-08-07 15:58:29 +0300

    Update book24.py

--- hw_books/book_parser/spiders/book24.py
+++ hw_books/book_parser/spiders/book24.py
@@ -8,12 +8,12 @@ class Book24Spider(scrapy.Spider):
     start_urls = ['https://book24.ru/search/?q=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5']
 
     def parse(self, response:HtmlResponse):
-        #next_page = response.xpath("//a[@class='catalog-pagination__item _text js-pagination-catalog-item']/@href").extract()[-1]
-        #if 'Далее' in response.xpath("//a[@class='catalog-pagination__item _text js-pagination-catalog-item']/text()").extract():
-        #    yield response.follow(next_page, callback=self.parse)
-        #books_links = response.xpath("//a[@class='book__title-link js-item-element ddl_product_link ']/@href").extract()
-        #for link in books_links:
-        #    yield response.follow(link, callback=self.book_pars)
+        next_page = response.xpath("//a[@class='catalog-pagination__item _text js-pagination-catalog-item']/@href").extract()[-1]
+        if 'Далее' in response.xpath("//a[@class='catalog-pagination__item _text js-pagination-catalog-item']/text()").extract():
+            yield response.follow(next_page, callback=self.parse)
+        books_links = response.xpath("//a[@class='book__title-link js-item-element ddl_product_link ']/@href").extract()
+        for link in books_links:
+            yield response.follow(link, callback=self.book_pars)
         pass
 
     def book_pars(self, response: HtmlResponse):
